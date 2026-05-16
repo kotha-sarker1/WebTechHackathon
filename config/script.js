d917b13c6fdf0e7b6d1037eb107f60ea08d7dbd2
aslikhan49141 <aslikhan49141@gmail.com>
2026-05-16 20:38:09 +0600

    ajax002

--- config/script.js
+++ config/script.js
@@ -7,6 +7,8 @@ function filterJobs(){
 
     xhttp.open('post', '../Controller/jobController.php', true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send('action=filterJobs&category_id=' + category_id + '&job_type=' + job_type + '&location=' + location + '&salary_keyword=' + salary_keyword);
+}
 
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
@@ -15,5 +17,51 @@ function filterJobs(){
         }
     }
 
-    xhttp.send('action=filterJobs&category_id=' + category_id + '&job_type=' + job_type + '&location=' + location + '&salary_keyword=' + salary_keyword);
+function searchJobs(){
+    const keyword = document.getElementById('searchKeyword').value;
+    const xhttp   = new XMLHttpRequest();
+
+    xhttp.open('post', '../Controller/jobController.php', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send('action=searchJobs&keyword=' + keyword);
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            const jobs = JSON.parse(this.responseText);
+            renderJobCards(jobs);
+        }
+    }
+
+    
+}  
+
+function renderJobCards(jobs){
+    const container = document.getElementById('jobsContainer');
+
+    if(jobs.length == 0){
+        container.innerHTML = "<p>No jobs found.</p>";
+        return;
+    }
+
+    let html = "";
+
+    jobs.forEach(function(job){
+        const isSaved    = job.is_saved == "yes";
+        const heartColor = isSaved ? "red" : "#aaa";
+        const heartIcon  = isSaved ? "&#9829;" : "&#9825;";
+
+        html += "<div class='job-card'>";
+        html += "<div class='job-card-top'>";
+        html += "<div class='job-card-info'>";
+        html += "<h3>" + escapeHtml(job.title) + "</h3>";
+        html += "<div class='company'>" + escapeHtml(job.company_name) + "</div>";
+        html += "<div class='meta'>" + escapeHtml(job.category_name) + " &nbsp;|&nbsp; " + escapeHtml(job.location) + " &nbsp;|&nbsp; " + escapeHtml(job.job_type) + "<br>Salary: " + escapeHtml(job.salary_range) + " &nbsp;|&nbsp; Deadline: " + escapeHtml(job.deadline) + "</div>";
+        html += "</div>";
+        html += "<button class='heart-btn' id='heartBtn_" + job.id + "' onclick='toggleSaveJob(" + job.id + ")' style='color:" + heartColor + ";'>" + heartIcon + "</button>";
+        html += "</div>";
+        html += "<div class='job-card-footer'>";
+        html += "<a class='btn-view' href='job_detail.php?id=" + job.id + "'>View Details</a>";
+        html += "</div></div>";
+    });
+    container.innerHTML = html;
 }
